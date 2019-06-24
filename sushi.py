e4c7953b9d1f2dad4fdd4c3c90a450041921857b
Your Name <1418794698a@gmail.com>
2019-06-24 11:23:04 +0800

    修改产品

--- sushi.py
+++ sushi.py
@@ -3,37 +3,39 @@ from linebot.exceptions import (InvalidSignatureError)
 from linebot.models import *
 
 def send_sushi():
-    data = {
-      "type": "text",
-      "text": "Select your favorite food category or send me your location!",
-      "quickReply": {
-        "items": [
-          {
-            "type": "action",
-            "imageUrl": "https://example.com/sushi.png",
-            "action": {
-              "type": "message",
-              "label": "Sushi",
-              "text": "Sushi"
-            }
-          },
-          {
-            "type": "action",
-            "imageUrl": "https://example.com/tempura.png",
-            "action": {
-              "type": "message",
-              "label": "Tempura",
-              "text": "Tempura"
-            }
-          },
-          {
-            "type": "action",
-            "action": {
-              "type": "location",
-              "label": "Send location"
-            }
-          }
-        ]
-      }
-    }
-    return data
+    message = TemplateSendMessage(
+        alt_text='图片轮播图',
+        template=ImageCarouselTemplate(
+            columns=[
+                ImageCarouselColumn(
+                    image_url="https://i.imgur.com/uKYgfVs.jpg",
+                    action=URITemplateAction(
+                        label="新鮮水果",
+                        uri="https://www.youtube.com/watch?v=1baqfP7g_Cg&feature=youtu.be"
+                    )
+                ),
+                ImageCarouselColumn(
+                    image_url="https://i.imgur.com/QOcAvjt.jpg",
+                    action=URITemplateAction(
+                        label="新鮮蔬菜",
+                        uri="https://cdn.101mediaimage.com/img/file/1410464751urhp5.jpg"
+                    )
+                ),
+                ImageCarouselColumn(
+                    image_url="https://i.imgur.com/Np7eFyj.jpg",
+                    action=URITemplateAction(
+                        label="可愛狗狗",
+                        uri="http://imgm.cnmo-img.com.cn/appimg/screenpic/big/674/673928.JPG"
+                    )
+                ),
+                ImageCarouselColumn(
+                    image_url="https://i.imgur.com/QRIa5Dz.jpg",
+                    action=URITemplateAction(
+                        label="可愛貓咪",
+                        uri="https://m-miya.net/wp-content/uploads/2014/07/0-065-1.min_.jpg"
+                    )
+                )
+            ]
+        )
+    )
+    return message
