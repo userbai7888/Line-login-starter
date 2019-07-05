a87d3a8424f1d3361ab7db390819180629734bfe
Your Name <1418794698a@gmail.com>
2019-07-05 10:35:26 +0800

    查看属性

--- app.py
+++ app.py
@@ -43,10 +43,9 @@ def callback():
 def handle_message(event):
     print(event,"*"*20)
     msg = event.message.text.encode('utf-8').decode('utf-8')
-    msg2 = event.message.id
-    user_id = event.source.userId.encode('utf-8').decode('utf-8')
-    print(msg2)
-    print(user_id,"userID 打印")
+    user_id = event.source
+    print(msg)
+    print(dir(user_id),"userID 打印")
     try:
         if msg in "我想購物":
             message = imagemap_message()
